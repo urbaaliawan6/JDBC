a3c81fa6deae3fd8d2751a2f4146f22dcbf538f8
Urba Awan <85040652+urbaaliawan6@users.noreply.github.com>
2021-11-01 13:25:43 -0700

    Add files via upload

---
+++ DBMS/all-queries/order.java
@@ -0,0 +1,40 @@
+package likeordertop;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class order {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+    
+		getConnection();
+	}
+    public static Connection getConnection () throws Exception{
+    	try {
+    		String driver="com.mysql.cj.jdbc.Driver";
+    		String url= "jdbc:mysql://127.0.0.1:3307/star";
+    		String username="root";
+    		String password="";
+    		Class.forName(driver);
+    		
+    		Connection conn=DriverManager.getConnection(url,username,password);
+    		//System.out.print("Connected");
+    		Statement s=conn.createStatement();
+    		ResultSet rs=s.executeQuery("select * from products1 order by seller_city='DESC'");
+    		while(rs.next())
+    			System.out.println(rs.getInt(1)+" "+rs.getString(2)+" "+rs.getString(3));
+    		conn.close();
+    		return conn;
+    	}
+    	catch(Exception e)
+    	{
+    System.out.println(e);
+}
+return null;
+}
+}
+    		
+    	 
