994a4d1c2d4c7af80c5f22f0e6c7d6e18a182012
Urba Awan <85040652+urbaaliawan6@users.noreply.github.com>
2021-11-01 13:35:05 -0700

    Add files via upload

---
+++ Object/object.java
@@ -0,0 +1,15 @@
+package object;
+
+class Student
+{
+ int a=9;
+ int b=8;
+ 
+}
+public class object{
+
+	public static void main(String[] args) {
+		Student s1= new Student();
+		System.out.println(s1.a);
+	}
+}
