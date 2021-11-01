374a2bebea9f51fa8c51b5c88292ef63ddaf1021
Urba Awan <85040652+urbaaliawan6@users.noreply.github.com>
2021-11-01 13:33:49 -0700

    Add files via upload

---
+++ HelloWorld/task.java
@@ -0,0 +1,20 @@
+package HelloWorld;
+
+public class task {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=21;
+		int y=25;
+		int z= x+y;
+		int u=x-y;
+		int s=x/y;
+		int a=x*y;
+		System.out.println(z);
+		System.out.println(u);
+		System.out.println(s);
+		System.out.println(a);
+
+	}
+
+}
